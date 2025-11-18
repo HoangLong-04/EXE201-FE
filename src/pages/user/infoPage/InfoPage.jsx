a38349d12d789a0e94a54309a12fc9a3efb65e92
Long Lê <longfg333@gmail.com>
2025-11-18 21:21:23 +0700

    fix

--- src/pages/user/infoPage/InfoPage.jsx
+++ src/pages/user/infoPage/InfoPage.jsx
@@ -1,19 +1,20 @@
 import UserInfo from "../../../components/userInfo/UserInfo";
-import Logo from "../../../assets/logo-exe.png";
 import { useAuth } from "../../../hooks/useAuth";
 
 function InfoPage() {
-  const { user } = useAuth()
-  
-  
+  const { user } = useAuth();
+
   return (
     <div className="bg-gray-50 flex justify-center items-center min-h-screen">
       <UserInfo
-        img={user.userProfile?.avatarUrl || "https://freesvg.org/img/abstract-user-flat-3.png"}
+        img={
+          user.userProfile?.avatarUrl ||
+          "https://freesvg.org/img/abstract-user-flat-3.png"
+        }
         fullName={user.userProfile?.fullName}
         id={user.userProfile?.id}
         phone={user.userProfile?.phone}
-        bankAcc={user.userProfile?.bankAcc || '0988'}
+        bankAcc={user.userProfile?.bankAcc || "0988"}
         email={user.userProfile?.email}
         address={user.userProfile?.address}
         school={user.userProfile?.school}
